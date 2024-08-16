64360a675df84d8a950202b9fdf85550dbe2d82e
Erik <Erik@LAPTOP-0S68ED0H>
2024-08-15 17:33:14 -0700

    Add project files.

---
+++ TicTacToe/TicTacToe.cpp
@@ -0,0 +1,119 @@
+// TicTacToe.cpp : Erik Rodriguez
+#include <iostream>
+using namespace std;
+char matrix[3][3] = { '1','2','3','4','5','6','7','8','9' };
+char player = 'X';
+// Displays Matrix of numbers
+void Draw()
+{
+	system("cls");
+	cout << "Tic Tac Toe" << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+//Input funtion to select a spot
+void input()
+{
+	int a;
+	cout << "Press the number of the field you want to fill: ";
+	cin >> a;
+
+	if (a == 1)
+		matrix[0][0] = player;
+	else if (a == 2)
+		matrix[0][1] = player;
+	else if (a == 3)
+		matrix[0][2] = player;
+	else if (a == 4)
+		matrix[1][0] = player;
+	else if (a == 5)
+		matrix[1][1] = player;
+	else if (a == 6)
+		matrix[1][2] = player;
+	else if (a == 7)
+		matrix[2][0] = player;
+	else if (a == 8)
+		matrix[2][1] = player;
+	else if (a == 9)
+		matrix[2][2] = player;
+}
+void toggleplayer()
+{
+	if (player == 'X')
+		player = 'O';
+	else
+		player = 'X';
+}
+//Win conditions
+char win()
+{// first player
+	// row win conditions
+	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	// column win conditions
+	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
+		return 'X';
+	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
+		return 'X';
+	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	// Diagonal Win conditions
+	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X')
+		return 'X';
+	//Second player
+	// row win conditions
+	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	// column win conditions
+	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
+		return 'O';
+	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+		return 'O';
+	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	// Diagonal Win conditions
+	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O')
+		return 'O';
+
+	return '/';
+	
+}
+int main()
+{
+
+	Draw();
+	while (1)
+	{
+		input();
+		Draw();
+		if (win() == 'X') {
+			cout << "X Wins!" << endl;
+			break;
+		}
+		else if (win() == 'O') {
+			cout << "O Wins!" << endl;
+			break;
+		}
+
+		toggleplayer();
+	}
+	system("pause");
+	return 0;
+}
+
